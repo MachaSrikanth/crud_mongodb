ff5f749ed95b7d201f38de769f0da1eef519b2d5
srikanth <srikanth.macha10@gamil.com>
2024-05-02 16:25:31 +0530

    updated package.json

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ app.get('/healthcheck',(req,res)=>{
     res.send("connected successfully.")
 })
 
-app.listen(3000,()=>{
-    console.log("server is running on 3000");
+app.listen(3001,()=>{
+    console.log("server is running on 3001");
 })
